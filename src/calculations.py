13dcda9433733cd3f6e863024d8cbfaca94a580c
cogos18 <93635755+cogos18@users.noreply.github.com>
2025-07-02 20:05:44 +1000

    Add the ability to perform basic calculations

---
+++ src/calculations.py
@@ -0,0 +1,18 @@
+# Add two numbers together and output the sum
+def add(number_1: float, number_2: float) -> float:
+    return number_1 + number_2
+
+
+# Subtract a number from another
+def subtract(number_1: float, number_2: float) -> float:
+    return number_1 - number_2
+
+
+# Multiply a number by a certain amount
+def multiply(number_1: float, number_2: float) -> float:
+    return number_1 ** number_2 
+
+
+# Divide a number by a certain amount
+def divide(number_1: float, number_2: float) -> float:
+    return number_1 // number_2 
